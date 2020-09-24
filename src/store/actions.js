8f93437e1c94000543362141bdcefdf9a6a0c149
Crowdbotics <team@crowdbotics.com>
2020-09-24 18:07:43 +0000

    Update store

--- src/store/actions.js
+++ src/store/actions.js
@@ -26,13 +26,16 @@ export const signupandlogin_post_api_v1_signup_readFailed = error => ({
   type: types.SIGNUPANDLOGIN_POST_API_V1_SIGNUP_READ_FAILED,
   error
 })
+
 export const registrationandlogin_post_registration_read = () => ({
   type: types.REGISTRATIONANDLOGIN_POST_REGISTRATION_READ
 })
+
 export const registrationandlogin_post_registration_readSucceeded = response => ({
   type: types.REGISTRATIONANDLOGIN_POST_REGISTRATION_READ_SUCCEEDED,
   response
 })
+
 export const registrationandlogin_post_registration_readFailed = error => ({
   type: types.REGISTRATIONANDLOGIN_POST_REGISTRATION_READ_FAILED,
   error
