c15a0895b458dc275969a5af9718edd0e6f5f13e
Crowdbotics <team@crowdbotics.com>
2020-09-24 20:29:22 +0000

    Update store

--- src/store/services.js
+++ src/store/services.js
@@ -14,8 +14,8 @@ const tstcr2020092101API = axios.create({
 function registrationandlogin_post_registration_read(requestBody) {
   return registrationandLogin.post(`/registration/`, requestBody)
 }
-function signupandlogin_post_api_v1_login_read(requestBody) {
-  return signupandLogin.post(`/api/v1/login`, requestBody)
+function signupandlogin_post_api_v1_login_read(password, username) {
+  return signupandLogin.post(`/api/v1/login/`)
 }
 function signupandlogin_post_api_v1_signup_read(requestBody) {
   return signupandLogin.post(`/api/v1/signup`, requestBody)
