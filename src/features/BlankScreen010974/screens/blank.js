4b5afbcd3a09775c15da93382b7f64ce257bf9fc
Crowdbotics <team@crowdbotics.com>
2020-09-28 21:19:09 +0000

    Update components

--- src/features/BlankScreen010974/screens/blank.js
+++ src/features/BlankScreen010974/screens/blank.js
@@ -20,7 +20,11 @@ export class __Blank extends React.Component {
     return { headerLeft: <SlideMenuIcon navigationProps={navigation} /> }
   }
   state = {}
-  render = () => <View style={this.props.themedStyle.View_1} />
+  render = () => (
+    <View style={this.props.themedStyle.View_1}>
+      <Text style={this.props.themedStyle.Text_3}>Sample text content</Text>
+    </View>
+  )
 }
 
 _Blank = withStyles(__Blank, theme => ({
@@ -31,7 +35,7 @@ _Blank = withStyles(__Blank, theme => ({
     marginBottom: 0,
     paddingLeft: 0,
     paddingRight: 0,
-    paddingTop: 10,
+    paddingTop: 0,
     paddingBottom: 0,
     overflow: "visible",
     fontSize: 12,
@@ -53,6 +57,36 @@ _Blank = withStyles(__Blank, theme => ({
     borderBottomWidth: 0,
     borderRadius: 0,
     backgroundSize: "auto"
+  },
+  Text_3: {
+    marginLeft: 0,
+    marginRight: 0,
+    marginTop: 5,
+    marginBottom: 5,
+    paddingLeft: 5,
+    paddingRight: 5,
+    paddingTop: 5,
+    paddingBottom: 5,
+    overflow: "visible",
+    fontSize: 12,
+    color: "#000000",
+    backgroundColor: "#ffffff",
+    fontStyle: "normal",
+    fontWeight: "normal",
+    borderColor: "#000000",
+    borderStyle: "solid",
+    borderWidth: 0,
+    borderLeftWidth: 0,
+    borderRightWidth: 0,
+    borderTopWidth: 0,
+    borderBottomWidth: 0,
+    borderRadius: 0,
+    textAlign: "left",
+    textAlignVertical: "center",
+    textDecorationLine: "none",
+    textTransform: "none",
+    lineHeight: 12,
+    letterSpacing: 0
   }
 }))
 
